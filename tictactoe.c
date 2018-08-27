34e94dc651f424a5f753c78c2df25038d89545dd
JunaidRahim <junaidrahim8d@gmail.com>
2018-08-28 01:01:57 +0530

    Done with C

--- tictactoe.c
+++ tictactoe.c
@@ -1,7 +1,100 @@
 #include <stdio.h>
+#include <stdbool.h>
+
+void print_matrix(char game[3][3]){ // just to print the game
+    for(int i=0; i<3; i++){
+        printf("%c | %c | %c \n", game[i][0], game[i][1], game[i][2]);
+    }
+}
+
+int check_winner(char game[3][3]){
+    
+    bool winO_diagonal1 = (game[0][0]=='O') && (game[1][1]=='O') && (game[2][2]=='O');
+    bool winO_diagonal2 = (game[0][2]=='O') && (game[1][1]=='O') && (game[2][0]=='O');
+
+    bool winX_diagonal1 = (game[0][0]=='X') && (game[1][1]=='X') && (game[2][2]=='X');
+    bool winX_diagonal2 = (game[0][2]=='X') && (game[1][1]=='X') && (game[2][0]=='X');
+    
+    if (winO_diagonal1 || winO_diagonal2){
+        return 1;
+    }
+    else if (winX_diagonal1 || winX_diagonal2){
+        return 2;
+    }
+    
+    for (int i=0; i<3; i++){
+
+        bool winO_horizontal = (game[i][0]=='O') && (game[i][1]=='O') && (game[i][2]=='O');
+        bool winO_vertical   = (game[0][i]=='O') && (game[1][i]=='O') && (game[2][i]=='O');
+
+        bool winX_horizontal = (game[i][0]=='X') && (game[i][1]=='X') && (game[i][2]=='X');
+        bool winX_vertical   = (game[0][i]=='X') && (game[1][i]=='X') && (game[2][i]=='X');
+
+        if (winO_horizontal || winO_vertical){
+            return 1;
+        }
+        else if (winX_horizontal || winX_vertical){
+            return 2;
+        }
+
+    }
+
+    return 0;
+}
 
 int main(){
+    char matrix[3][3];
 
+    for(int i=0; i<3; i++){ // Filling up the matrix
+        matrix[i][0]=' ';
+        matrix[i][1]=' ';
+        matrix[i][2]=' ';
+    }
+
+    for(int t=0; t<9; t++){
+
+        if(t%2==0){
+            // Even Turn, O
+            int row, col;
+            
+            printf("Enter Row and Column(O): ");
+            scanf("%d %d", &row, &col);
+
+            matrix[row][col] = 'O';
+            print_matrix(matrix);
+
+            int result = check_winner(matrix);
+            if(result==1){
+                printf("O Won\n");
+                break;
+            }
+            else if(result==2){
+                printf("X Won\n");
+                break;
+            }   
+        }
+        else{
+            // Odd Turn, X
+            int row, col;
+            
+            printf("Enter Row and Column(X): ");
+            scanf("%d %d", &row, &col);
+
+            matrix[row][col] = 'X';
+            print_matrix(matrix);
+
+            int result = check_winner(matrix);
+            if(result==1){
+                printf("O Won\n");
+                break;
+            }
+            else if(result==2){
+                printf("X Won\n");
+                break;
+            }    
+        }
+
+    }
 
     return 0;
 }
